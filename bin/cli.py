bd6984155d8d861257eeb24378cfc9882d87edb1
CDTAdmin <cdtadmin@ndmpc1053.ndm.ox.ac.uk>
2022-09-30 10:16:27 +0100

    hyperparameters via cli

--- bin/cli.py
+++ bin/cli.py
@@ -19,12 +19,24 @@ def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+
+    epochs: int = typer.Option(
+        ..., "-e", "--epochs", help="Number of epochs"
+    ),
+
+    batch_size: int = typer.Option(
+        ..., "-b", "--batch_size", help="Batch size"
+    ),
+
+    learning_rate: float = typer.Option(
+        ..., "-r", "--learning_rate", help="Learning rate"
+    ),
+
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
+    #batch_size = 1000
+    #learning_rate = 0.01
 
     # save the parameters!
 
